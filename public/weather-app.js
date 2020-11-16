69ddbede75a20a7fe28dc31d99219ba6486c6d98
Malik Faizan Hameed <malik.faizan18@gmail.com>
2020-11-17 01:30:08 +0500

    using API from backend

--- public/weather-app.js
+++ public/weather-app.js
@@ -4,57 +4,69 @@ const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
 const list = document.querySelector(".ajax-section .cities");
 
-// API Key
-const apiKey = 'f84009647ce2a9db9dcff46b58632a4b';
+// // API Key
+// const apiKey = 'f84009647ce2a9db9dcff46b58632a4b';
 
 //Using fetch POST method
-const data1 = {
-  abc: 'Hello',
-  xyz: 'Bye'
-};
-const options = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(data1)
-};
-fetch('/api', options);
+// const city = input.value;
+// const options = {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify(city)
+// };
+// fetch('/api', options);
 
 //Adding Event Listener
 form.addEventListener("submit", async e => {
-    
+
   //Prevent Default Behavior
   e.preventDefault();
 
+  const city = {
+    name: input.value
+  };
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(city)
+  };
+  
+// debugger;
   //try to ask openweather for data
   try {
-      //select city entered
-      const inputVal = input.value;
-      //API call
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&units=metric&appid=${apiKey}`;
-      
-      //wait for response and convert into JSON format
-      const resp = await fetch(url);
-      const data = await resp.json();
-      //create list element with response data
-      createList(data);
-      //reset with success
-      reset(' ');
-  } catch {
-      //reset with error message
-      reset('Please enter a valid city name!');
+    const resp = await fetch('/api', options);
+    const data = await resp.json();
+    //select city entered
+    // const inputVal = input.value;
+    //API call
+    // const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&units=metric&appid=${apiKey}`;
+
+    //wait for response and convert into JSON format
+    // const resp = await fetch(url);
+    // const data = await resp.json();
+    //create list element with response data
+    createList(data);
+    //reset with success
+    reset(' ');
+  } catch(e) {
+    console.log(e);
+    //reset with error message
+    reset('Please enter a valid city name!');
   };
 });
 
-function reset(message){
+function reset(message) {
   msg.textContent = message;
   form.reset();
   input.focus();
 }
-function createList(data){
+function createList(data) {
   //destructuring data
-  const {main, sys, weather, name} = data;
+  const { main, sys, weather, name } = data;
   //setting icon
   const icon = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
   //adding HTML
@@ -65,8 +77,7 @@ function createList(data){
   </h2>
   <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
   <figure>
-      <img class="city-icon" src="${icon}" alt="${
-      weather[0].description}">
+      <img class="city-icon" src="${icon}" alt="${weather[0].description}">
       <figcaption>${weather[0].description}</figcaption>
   </figure>`;
   //create list
--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
 
+const axios = require('axios').default;
+
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
 
-app.post('/api', (req, res) => {
+// API Key
+const apiKey = 'f84009647ce2a9db9dcff46b58632a4b';
+
+
+app.post('/api', async (req, res) => {
     console.log(req.body);
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${req.body.name}&units=metric&appid=${apiKey}`;
+
+    //wait for response and convert into JSON format
+    const resp = await axios.get(url);
+    // const data = await resp.json();
+    res.json(resp.data);
+
+    // console.log(resp.data);
+
 });
 app.get('/', (req, res) => {
     res.sendFile('index.html');
